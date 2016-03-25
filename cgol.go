01778d8ac501008ed915b0ac7897557db5c0209c
JohnnyDevOps <john@xonk.org>
2016-03-25 17:51:27 -0600

    almost done

--- cgol.go
+++ cgol.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"reflect"
 	"time"
 )
 
-func newWorld() world {
+const width int = 20
+const height int = 20
+const prob int = 10
+
+// MAIN ///////////////////////////////////////////
+func main() {
+	//fmt.Println(width, height)
+	var gameOfLife world
+	gameOfLife = newWorld(prob)
+	gameOfLife.display()
+	cont()
+	for {
+		gameOfLife.step()
+		time.Sleep(1000 * time.Millisecond)
+		gameOfLife.display()
+	}
+}
+
+func newWorld(prob int) world {
 	var new world
 	for c := 1; c <= height; c++ {
 		var row []cell
@@ -23,25 +43,54 @@ func newWorld() world {
 	return new
 }
 
-func (w world) display() {
-	for _, r := range w {
-		for _, c := range r {
-			switch c.alive {
-			case true:
-				fmt.Printf("[]")
-			case false:
-				fmt.Printf("  ")
+type world [][]cell
+
+func (w world) step() world {
+	next := newWorld(0)
+	// For each cell assign neighbor cells +1
+	for x, _ := range w {
+		for y, _ := range w[x] {
+			nbrs := coords{x, y}.myNeighbors()
+
+			//fmt.Println("list neighbors for:	", coords{x, y})
+			//fmt.Println("unfiltered neighbors:	", nbrs)
+			for _, n := range nbrs {
+				if n.x >= 0 && n.x < width && n.y >= 0 && n.y < height {
+					//fmt.Println(n.x, n.y)
+					next[n.x][n.y].neighbors++
+				}
 			}
+			//fmt.Println(next)
+			//cont()
 		}
-		fmt.Println()
 	}
+
+	// set dead or alive
+	for x, _ := range w {
+		for y, _ := range w[x] {
+			// set alive here
+			next[x][y].alive = alive(next[x][y].alive, next[x][y].neighbors)
+		}
+	}
+
+	//fmt.Println("all", next)
+	return next
 }
 
-// MAIN ///////////////////////////////////////////
-func main() {
-	fmt.Println(width, height)
-	smorg := newWorld()
-	smorg.display()
+func alive(alive bool, neighbors int) bool {
+	if alive == true {
+		switch {
+		case neighbors < 2:
+			return false
+		case neighbors > 1 && neighbors < 4:
+			return true
+		case neighbors > 3:
+			return false
+		}
+	} else if alive == false && neighbors == 3 {
+		return true
+	}
+	return false
 }
 
 type coords struct {
@@ -54,7 +103,7 @@ type cell struct {
 	neighbors int
 }
 
-func (c coords) neighbors() []coords {
+func (c coords) myNeighbors() []coords {
 	var xn = []int{c.x - 1, c.x, c.x + 1}
 	var yn = []int{c.y - 1, c.y, c.y + 1}
 
@@ -73,12 +122,6 @@ func (c coords) neighbors() []coords {
 	return neighbors
 }
 
-const width int = 20
-const height int = 20
-const prob int = 10
-
-type world [][]cell
-
 func roll(n int, d int) int {
 	num := 0
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -87,4 +130,33 @@ func roll(n int, d int) int {
 	}
 	num = num / n
 	return num + 1
+}
+
+func (w world) display() {
+	clearscreen()
+	for _, r := range w {
+		for _, c := range r {
+			switch c.alive {
+			case true:
+				fmt.Printf("[]")
+			case false:
+				fmt.Printf("  ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func cont() {
+	fmt.Printf("Press enter to continue: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	choice := string([]byte(input)[0])
+	switch choice {
+	default:
+		return
+	}
+}
+func clearscreen() {
+	fmt.Printf("[H[J")
 }
