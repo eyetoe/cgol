4b260176ba8679ac442f9e47ef20f561acd1ef02
JohnnyDevOps <john@xonk.org>
2016-03-25 23:40:58 -0600

    working yay!

--- cgol.go
+++ cgol.go
@@ -9,22 +9,105 @@ import (
 	"time"
 )
 
-const width int = 20
-const height int = 20
-const prob int = 10
+const width int = 80
+const height int = 80
+const prob int = 60
 
 // MAIN ///////////////////////////////////////////
 func main() {
-	//fmt.Println(width, height)
-	var gameOfLife world
-	gameOfLife = newWorld(prob)
-	gameOfLife.display()
+
+	new := newWorld(prob)
+	new.display()
 	cont()
-	for {
-		gameOfLife.step()
-		time.Sleep(1000 * time.Millisecond)
-		gameOfLife.display()
+	for i := 0; true; i++ {
+		new = new.step()
+		new.display()
+		fmt.Println("iteration:", i)
+		time.Sleep(200 * time.Millisecond)
 	}
+
+	//fmt.Println(checkAlive(false, 3))
+
+	//		time.Sleep(1000 * time.Millisecond)
+}
+
+func myNeighbors(c coords) []coords {
+	var xn = []int{c.x - 1, c.x, c.x + 1}
+	var yn = []int{c.y - 1, c.y, c.y + 1}
+
+	var neighbors []coords
+
+	for _, a := range xn {
+		for _, b := range yn {
+			// reflect.DeepEqual is often wrong to use
+			// but in this context there is very low variability
+			// though it may still be wrong.
+			if !reflect.DeepEqual(c, coords{a, b}) {
+				neighbors = append(neighbors, coords{a, b})
+			}
+		}
+	}
+	return neighbors
+}
+
+func (w world) step() world {
+
+	// zero out neighbor values
+	for x, _ := range w {
+		for y, _ := range w[x] {
+			w[x][y].neighbors = 0
+		}
+	}
+	// For each cell assign neighbor cells +1
+	for xi, x := range w {
+		for yi, _ := range x {
+			if w[xi][yi].alive == true {
+				nbrs := myNeighbors(coords{xi, yi})
+				//fmt.Println(coords{xi, yi}, "neighbors are:", nbrs)
+
+				for _, n := range nbrs {
+					if n.x >= 0 && n.x < width && n.y >= 0 && n.y < height {
+						w[n.x][n.y].neighbors = w[n.x][n.y].neighbors + 1
+
+					}
+				}
+			}
+		}
+	}
+	// set dead or alive
+	for xi, x := range w {
+		for yi, _ := range x {
+			// set alive here
+			//fmt.Println(w[xi][yi])
+			w[xi][yi].alive = checkAlive(w[xi][yi].alive, w[xi][yi].neighbors)
+			//fmt.Printf("starting vals: %t, %d => new val: %t\n", w[xi][yi].alive, w[xi][yi].neighbors, checkAlive(w[xi][yi].alive, w[xi][yi].neighbors))
+		}
+	}
+	return w
+}
+
+func checkAlive(alive bool, neighbors int) bool {
+	switch alive {
+	case true:
+		switch {
+		case neighbors < 2:
+			return false
+		case neighbors > 1 && neighbors < 4:
+			return true
+		case neighbors > 3:
+			return false
+		}
+	case false:
+		switch {
+		case neighbors == 3:
+			return true
+		default:
+			return false
+		}
+	default:
+		return false
+	}
+	return false
 }
 
 func newWorld(prob int) world {
@@ -45,52 +128,27 @@ func newWorld(prob int) world {
 
 type world [][]cell
 
-func (w world) step() world {
-	next := newWorld(0)
-	// For each cell assign neighbor cells +1
-	for x, _ := range w {
-		for y, _ := range w[x] {
-			nbrs := coords{x, y}.myNeighbors()
-
-			//fmt.Println("list neighbors for:	", coords{x, y})
-			//fmt.Println("unfiltered neighbors:	", nbrs)
-			for _, n := range nbrs {
-				if n.x >= 0 && n.x < width && n.y >= 0 && n.y < height {
-					//fmt.Println(n.x, n.y)
-					next[n.x][n.y].neighbors++
-				}
+func (w world) display() {
+	clearscreen()
+	// debug
+	//	for xi, x := range w {
+	//		for yi, y := range x {
+	//			fmt.Printf("%d,%d %t	", xi, yi, y.alive)
+	//		}
+	//		fmt.Println()
+	//	}
+	// end debug
+	for _, x := range w {
+		for _, y := range x {
+			switch y.alive {
+			case true:
+				fmt.Printf("[]")
+			case false:
+				fmt.Printf("  ")
 			}
-			//fmt.Println(next)
-			//cont()
 		}
+		fmt.Println()
 	}
-
-	// set dead or alive
-	for x, _ := range w {
-		for y, _ := range w[x] {
-			// set alive here
-			next[x][y].alive = alive(next[x][y].alive, next[x][y].neighbors)
-		}
-	}
-
-	//fmt.Println("all", next)
-	return next
-}
-
-func alive(alive bool, neighbors int) bool {
-	if alive == true {
-		switch {
-		case neighbors < 2:
-			return false
-		case neighbors > 1 && neighbors < 4:
-			return true
-		case neighbors > 3:
-			return false
-		}
-	} else if alive == false && neighbors == 3 {
-		return true
-	}
-	return false
 }
 
 type coords struct {
@@ -103,25 +161,6 @@ type cell struct {
 	neighbors int
 }
 
-func (c coords) myNeighbors() []coords {
-	var xn = []int{c.x - 1, c.x, c.x + 1}
-	var yn = []int{c.y - 1, c.y, c.y + 1}
-
-	var neighbors []coords
-
-	for _, a := range xn {
-		for _, b := range yn {
-			// reflect.DeepEqual is often wrong to use
-			// but in this context there is very low variability
-			// though it may still be wrong.
-			if !reflect.DeepEqual(c, coords{a, b}) {
-				neighbors = append(neighbors, coords{a, b})
-			}
-		}
-	}
-	return neighbors
-}
-
 func roll(n int, d int) int {
 	num := 0
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -130,21 +169,6 @@ func roll(n int, d int) int {
 	}
 	num = num / n
 	return num + 1
-}
-
-func (w world) display() {
-	clearscreen()
-	for _, r := range w {
-		for _, c := range r {
-			switch c.alive {
-			case true:
-				fmt.Printf("[]")
-			case false:
-				fmt.Printf("  ")
-			}
-		}
-		fmt.Println()
-	}
 }
 
 func cont() {
